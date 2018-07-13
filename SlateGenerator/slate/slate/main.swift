18031cd8198d16fb8f250f2e4c7a12d44a0d0c21
Jason Fieldman <jason@fieldman.org>
2018-07-13 09:10:41 -0700

    main

--- SlateGenerator/slate/slate/main.swift
+++ SlateGenerator/slate/slate/main.swift
@@ -8,16 +8,18 @@
 
 import Foundation
 
+var _useInt: Bool = false
 
 //try?
 command(
     Argument<String>("modelPath", description: "Path to the xcdatamodel file"),
     Argument<String>("outputPath", description: "Directory to write generated files"),
     Option<Int>("useclass", default: 0, description: "0 to use struct, 1 to use class"),
+    Option<Int>("useint", default: 0, description: "0 to use declared values (Int16, etc), 1 to force Int"),
     Option<String>("name", default: "Slate%@", description: "Immutable class name transform; %@ is replaced by Entity name."),
     Option<String>("file", default: "", description: "File name transform; %@ is replaced by Entity name.  No %@ puts all classes in one file."),
     Option<String>("import", default: "", description: "Import an additional swift module")
-) { modelPath, outputPath, useclass, classXform, fileXform, importModule in
+) { modelPath, outputPath, useclass, useint, classXform, fileXform, importModule in
 
     let contentsPath = ((modelPath as NSString).expandingTildeInPath as NSString).appendingPathComponent("contents")
     if !FileManager.default.fileExists(atPath: contentsPath) {
@@ -35,7 +37,8 @@ command(
     
     let realFileXform = (fileXform == "") ? classXform : fileXform
     let shouldUseClass = useclass != 0
-    
+    _useInt = useint != 0
+
     let entities = ParseCoreData(contentsPath: contentsPath)
     CoreDataSwiftGenerator.generateCoreData(entities: entities,
                                             useClass: shouldUseClass,
@@ -46,5 +49,5 @@ command(
     
 }.run(
     //["/Users/jasonfieldman/Development/Slate_Start/SlatePlayground/SlatePlayground/SlatePlayground.xcdatamodeld/SlatePlayground 2.xcdatamodel",
-    //"/tmp/"]
+    //"/Users/jasonfieldman/Development/Slate_Start/SlatePlayground/SlatePlayground/CoreData/"]
 )
